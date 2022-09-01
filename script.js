b7c00c12b263d563050f447029f41e3df3d734c6
Joel Singh <joelsingh788@gmail.com>
2022-09-01 18:58:50 -0400

    Add .password-matching-text

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const passwordInputs = document.querySelectorAll("input[type='password']");
   element.addEventListener("keyup", (e) => {
     if (passwordInputs[0].value === passwordInputs[1].value) {
       passwordInputs.forEach((e) => e.classList.add("matching"));
+      document
+        .querySelector(".password-matching-text")
+        .setAttribute("hidden", "");
     } else {
       passwordInputs.forEach((e) => e.classList.remove("matching"));
+      document
+        .querySelector(".password-matching-text")
+        .removeAttribute("hidden");
     }
   });
 });
